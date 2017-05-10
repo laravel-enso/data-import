03118bc8168a29b17181dc7d1661bbe8d5cb6693
aocneanu <aocneanu@gmail.com>
2017-05-10 10:55:06 +0000

    Apply fixes from StyleCI

--- src/app/Enums/ComplexValidationTypesEnum.php
+++ src/app/Enums/ComplexValidationTypesEnum.php
@@ -15,8 +15,8 @@ class ComplexValidationTypesEnum extends AbstractEnum
     public function __construct()
     {
         $this->data = [
-            'exists_in_sheet'  => __('Value must exist in the sheet'),
-            'unique_in_column' => __('Value must be unique in its column'),
+            'exists_in_sheet'   => __('Value must exist in the sheet'),
+            'unique_in_column'  => __('Value must be unique in its column'),
             'duplicate_lines'   => __('Sheet lines must be unique'),
         ];
     }
