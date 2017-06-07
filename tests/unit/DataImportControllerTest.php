1933a7c36e2de1b9303a5b0ac17d22a71f995d72
Mihai <utopic_dude@yahoo.co.uk>
2017-06-07 11:29:57 +0300

    tests cleanup

--- tests/unit/DataImportControllerTest.php
+++ tests/unit/DataImportControllerTest.php
@@ -13,6 +13,8 @@ class DataImportControllerTest extends TestCase
     private $user;
     /** @var DataImportController */
     private $diController;
+    private $testFilePath;
+    private $basePath;
 
     protected function setUp()
     {
@@ -20,30 +22,8 @@ class DataImportControllerTest extends TestCase
 
         $this->user = User::first();
         $this->diController = new DataImportController();
-    }
-
-    /** @test */
-    public function can_delete_data_import()
-    {
-        // Arrange
-        // authenticate as first user
-        $this->be($this->user);
-        $dataImport = DataImport::create([
-            'type'          => 1,
-            'original_name' => 'Test Original Name.pdf',
-            'saved_name'    => 'testSavedName.pdf',
-            'comment'       => 'test comment',
-            'summary'       => '{}',
-        ]);
-
-        // Act
-        $result = $this->diController->destroy($dataImport);
-
-        // Assert
-        // deletion was successful, without errors
-        $this->assertEquals('success', $result->level);
-        $this->assertEquals(0, $result->errors->count());
-        $this->assertNull(DataImport::find($dataImport->id));
+        $this->testFilePath = __DIR__ . '/../testFiles/example_import_file.xlsx';
+        $this->basePath = __DIR__ . '/../testFiles/';
     }
 
     /** @test */
@@ -63,5 +43,58 @@ class DataImportControllerTest extends TestCase
         $response = $this->diController->getSummary($dataImport);
         $this->assertContains('summary', array_keys($response));
         $this->assertEquals($testSummary, $response['summary']);
+    }
+
+    /** @test */
+    public function can_import() {
+
+        $response = $this->createImport();
+
+        //evaluate
+        $respObject = json_decode($response->getContent());
+        $this->assertFalse($respObject->summary->hasErrors);
+        $this->assertEquals(5, $respObject->summary->successfulEntries);
+    }
+
+    /** @test */
+    public function can_destroy_import() {
+
+        $this->createImport();
+
+        \Log::debug(DataImport::all());
+
+        $dataImport = DataImport::find(1);
+        $result = $this->diController->destroy($dataImport);
+
+        $this->assertEquals('success', $result->level);
+        $this->assertEquals(0, $result->errors->count());
+        $this->assertNull(DataImport::find($dataImport->id));
+    }
+
+    private function createTempFile($path) {
+
+        $randomPrefix = mt_rand(100,1000);
+        $tempFilePath = $this->basePath . $randomPrefix . 'temp.xlsx';
+        copy($path, $tempFilePath);
+
+        return $tempFilePath;
+    }
+
+    private function createImport() {
+
+        $temporaryFilePath = $this->createTempFile($this->testFilePath);
+
+        //setup
+        $name = 'original_file_name.xlsx';
+        $path = $temporaryFilePath;
+        $mime = 'application/vnd.ms-excel';
+        //this file gets automatically deleted at the end of the test by Laravel
+        $file = new \Illuminate\Http\UploadedFile($path, $name, $mime, filesize($path), null, true);
+
+        //run
+        $this->be($this->user);
+        $response = $this->call('POST', 'import/run', ['type' => 0, 'enctype'=>"multipart/form-data", 'error'=>0], [], ['file_0' => $file]);
+
+        return $response;
     }
 }
