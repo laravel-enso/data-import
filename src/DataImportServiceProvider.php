d3c09ab149b4d46e6706adb931d1d0c957816957
mishu <mishu@xtelecom.ro>
2017-07-01 18:56:41 +0300

    closes #5

--- src/DataImportServiceProvider.php
+++ src/DataImportServiceProvider.php
@@ -6,31 +6,34 @@ use Illuminate\Support\ServiceProvider;
 
 class DataImportServiceProvider extends ServiceProvider
 {
-    /**
-     * Bootstrap the application services.
-     *
-     * @return void
-     */
     public function boot()
+    {
+        $this->publishesAll();
+        $this->loadDependencies();
+    }
+
+    private function publishesAll()
     {
         $this->publishes([
             __DIR__.'/config/importing.php' => config_path('importing.php'),
-        ], 'data-import-config');
+        ], 'dataimport-config');
+
+        $this->publishes([
+            __DIR__.'/config/importing.php' => config_path('importing.php'),
+        ], 'enso-config');
 
         $this->publishes([
             __DIR__.'/resources/classes' => app_path(),
         ], 'data-import-classes');
+    }
 
+    private function loadDependencies()
+    {
+        $this->mergeConfigFrom(__DIR__.'/config/importing.php', 'importing.php');
         $this->loadRoutesFrom(__DIR__.'/routes/web.php');
-        $this->loadMigrationsFrom(__DIR__.'/database/migrations');
         $this->loadViewsFrom(__DIR__.'/resources/views', 'laravel-enso/data-import');
     }
 
-    /**
-     * Register the application services.
-     *
-     * @return void
-     */
     public function register()
     {
         //
