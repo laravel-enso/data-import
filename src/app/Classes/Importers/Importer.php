b346b6a2a8d42d13f8e64619fdd2c5b414f8237d
mishu <mishu@xtelecom.ro>
2017-09-10 13:35:49 +0300

    add trimContents to Importer. Remove BaseValidator

--- src/app/Classes/Importers/Importer.php
+++ src/app/Classes/Importers/Importer.php
@@ -4,45 +4,57 @@ namespace LaravelEnso\DataImport\app\Classes\Importers;
 
 use LaravelEnso\DataImport\app\Classes\ImportConfiguration;
 use LaravelEnso\DataImport\app\Classes\Reporting\ImportSummary;
-use LaravelEnso\DataImport\app\Classes\Validators\BaseValidator;
+use LaravelEnso\DataImport\app\Classes\Validators\ContentValidator;
+use LaravelEnso\DataImport\app\Classes\Validators\StructureValidator;
 
 class Importer
 {
+    protected $sheets;
     protected $summary;
-    protected $validator;
+    protected $structureValidator;
+    protected $contentValidator;
     protected $importer;
     protected $skipsContentErrors;
 
     public function __construct(string $type, $file)
     {
-        $config = new ImportConfiguration($type);
-        $sheets = $this->loadXlsx($file['full_path']);
+        $config       = new ImportConfiguration($type);
+        $this->sheets = $this->loadXlsx($file['full_path']);
 
         $this->skipsContentErrors = !$config->getStopOnErrors();
-        $this->summary = new ImportSummary($file['original_name']);
-        $this->validator = new BaseValidator($config, $sheets, $this->summary);
-        $this->importer = $config->getImporter($sheets, $this->summary);
+        $this->summary            = new ImportSummary($file['original_name']);
+        $this->structureValidator = new StructureValidator($config, $this->sheets, $this->summary);
+        $this->contentValidator   = new ContentValidator($config, $this->sheets, $this->summary);
+        $this->importer           = $config->getImporter($this->sheets, $this->summary);
     }
 
     public function run()
     {
-        $this->validator->run();
+        $this->structureValidator->run();
 
-        if ($this->validator->isValid() || $this->canRunWithErrors()) {
+        if ($this->summary->hasStructureErrors()) {
+            return false;
+        }
+
+        $this->trimContents();
+
+        $this->contentValidator->run();
+
+        if (!$this->summary->hasErrors() || $this->canRunWithErrors()) {
             $this->importer->run();
         }
     }
 
     public function fails()
     {
-        return $this->validator->hasStructureErrors()
-            || ($this->validator->hasContentErrors() && !$this->skipsContentErrors);
+        return $this->summary->hasStructureErrors()
+            || ($this->summary->hasContentErrors() && !$this->skipsContentErrors);
     }
 
     public function canRunWithErrors()
     {
-        return !$this->validator->hasStructureErrors()
-            && $this->skipsContentErrors;
+        return !$this->summary->hasStructureErrors()
+        && $this->skipsContentErrors;
     }
 
     public function getSummary()
@@ -53,5 +65,16 @@ class Importer
     private function loadXlsx($file)
     {
         return \Excel::load($file)->get();
+    }
+
+    private function trimContents()
+    {
+        $this->sheets->each(function ($sheet) {
+            $sheet->each(function ($row) {
+                foreach ($row as $key => $value) {
+                    $row[$key] = trim($value);
+                }
+            });
+        });
     }
 }
