087a5996f3de6c0f660c293d8c5df520f7e1ab9a
Mihai <mihai.ocneanu@gmail.com>
2017-09-25 11:31:42 +0300

    removed excel service provider manual loading in favor of package auto-discovery

--- src/DataImportServiceProvider.php
+++ src/DataImportServiceProvider.php
@@ -4,7 +4,6 @@ namespace LaravelEnso\DataImport;
 
 use Illuminate\Support\ServiceProvider;
 use LaravelEnso\DataImport\app\Classes\Template;
-use Maatwebsite\Excel\ExcelServiceProvider;
 
 class DataImportServiceProvider extends ServiceProvider
 {
@@ -46,7 +45,5 @@ class DataImportServiceProvider extends ServiceProvider
         $this->app->singleton('Template', function ($app, $params) {
             return new Template($params['template']);
         });
-
-        $this->app->register(ExcelServiceProvider::class);
     }
 }
