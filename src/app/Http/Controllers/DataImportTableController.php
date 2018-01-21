4bc15c99c13390bf2062e6f334a82a1984e78be3
mishu <mishu@xtelecom.ro>
2018-01-21 20:10:39 +0200

    fixes #12

--- src/app/Http/Controllers/DataImportTableController.php
+++ src/app/Http/Controllers/DataImportTableController.php
@@ -15,9 +15,9 @@ class DataImportTableController extends Controller
 
     public function query()
     {
-        return DataImport::select(\DB::raw('data_imports.id, data_imports.id as "dtRowId", data_imports.type,
+        return DataImport::select(\DB::raw("data_imports.id, data_imports.id as dtRowId, data_imports.type,
                 data_imports.original_name, data_imports.comment, data_imports.created_at,
-                concat(users.first_name, " ", users.last_name) as created_by'))
+                concat(users.first_name, ' ', users.last_name) as created_by"))
             ->join('users', 'data_imports.created_by', '=', 'users.id');
     }
 }
