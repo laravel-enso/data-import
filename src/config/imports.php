157c9c0be022076c0f4f786f598def57eca6cac9
mishu <mishu@xtelecom.ro>
2018-02-04 10:03:34 +0200

    removes deprecated params from config

--- src/config/imports.php
+++ src/config/imports.php
@@ -4,9 +4,5 @@ return [
     'example' => [
         'label' => 'Example import',
         'template' => 'app/Imports/Templates/exampleTemplate.json',
-        'importerClass' => 'App\\Imports\\Importers\\ExampleImporter',
-        'validatorClass' => 'App\\Imports\\Validators\\CustomValidator', // optional
-        'entryLimit' => 5000, // optional, per sheet, the default value is 5000
-        'stopsOnIssues' => false, // optional, the default value is false
     ],
 ];
