d98f1b4f3938e99ea42de36b13f147d4d6787bb4
mishu <mishu@xtelecom.ro>
2018-04-03 18:43:38 +0300

    updates for the new datatable use

--- src/app/Http/Controllers/DataImportTableController.php
+++ src/app/Http/Controllers/DataImportTableController.php
@@ -4,20 +4,12 @@ namespace LaravelEnso\DataImport\App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use LaravelEnso\VueDatatable\app\Traits\Excel;
-use LaravelEnso\DataImport\app\Models\DataImport;
 use LaravelEnso\VueDatatable\app\Traits\Datatable;
+use LaravelEnso\DataImport\app\Tables\Builders\DataImportTable;
 
 class DataImportTableController extends Controller
 {
     use Datatable, Excel;
 
-    private const Template = __DIR__.'/../../Tables/dataImports.json';
-
-    public function query()
-    {
-        return DataImport::select(\DB::raw("data_imports.id, data_imports.id as dtRowId, data_imports.type,
-                data_imports.original_name, data_imports.comment, data_imports.created_at,
-                concat(users.first_name, ' ', users.last_name) as created_by"))
-            ->join('users', 'data_imports.created_by', '=', 'users.id');
-    }
+    protected $tableClass = DataImportTable::class;
 }
---
+++ src/app/Tables/Builders/DataImportTable.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace LaravelEnso\DataImport\app\Tables\Builders;
+
+use LaravelEnso\VueDatatable\app\Classes\Table;
+use LaravelEnso\DataImport\app\Models\DataImport;
+
+class DataImportTable extends Table
+{
+    protected $templatePath = __DIR__.'/../Templates/dataImports.json';
+
+    public function query()
+    {
+        return DataImport::select(\DB::raw("data_imports.id, data_imports.id as dtRowId, data_imports.type,
+                data_imports.original_name, data_imports.comment, data_imports.created_at,
+                concat(users.first_name, ' ', users.last_name) as created_by"))
+            ->join('users', 'data_imports.created_by', '=', 'users.id');
+    }
+}
