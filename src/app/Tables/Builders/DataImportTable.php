e23d0c663e44e8ea6f4d891ff218990c2db760b3
mishu <mishu@xtelecom.ro>
2018-05-08 14:19:27 +0300

    fixes #14

--- src/app/Tables/Builders/DataImportTable.php
+++ src/app/Tables/Builders/DataImportTable.php
@@ -11,7 +11,7 @@ class DataImportTable extends Table
 
     public function query()
     {
-        return DataImport::select(\DB::raw("data_imports.id, data_imports.id as dtRowId, data_imports.type,
+        return DataImport::select(\DB::raw("data_imports.id, data_imports.id as  \"dtRowId\", data_imports.type,
                 data_imports.original_name, data_imports.comment, data_imports.created_at,
                 concat(users.first_name, ' ', users.last_name) as created_by"))
             ->join('users', 'data_imports.created_by', '=', 'users.id');
