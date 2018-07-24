22f24c055319c48d23fba03f17ac0636485a3c4e
mishu <mishu@xtelecom.ro>
2018-07-24 08:48:20 +0300

    visual refactor in table builder

--- src/app/Tables/Builders/DataImportTable.php
+++ src/app/Tables/Builders/DataImportTable.php
@@ -11,8 +11,8 @@ class DataImportTable extends Table
 
     public function query()
     {
-        return DataImport::select(\DB::raw("data_imports.id, data_imports.id as  \"dtRowId\", data_imports.type,
-                data_imports.original_name, data_imports.created_at,
+        return DataImport::select(\DB::raw("data_imports.id, data_imports.id as \"dtRowId\",
+                data_imports.type, data_imports.original_name, data_imports.created_at,
                 concat(users.first_name, ' ', users.last_name) as created_by"))
             ->join('users', 'data_imports.created_by', '=', 'users.id');
     }
