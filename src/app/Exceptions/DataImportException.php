f671be4ca7fcf530ca5c47a0201863cfa47fb0be
mishu <mishu@xtelecom.ro>
2019-09-18 10:35:52 +0300

    fixes exception methods

--- src/app/Exceptions/DataImportException.php
+++ src/app/Exceptions/DataImportException.php
@@ -8,17 +8,17 @@ class DataImportException extends EnsoException
 {
     public static function configNotReadable()
     {
-        throw new static(__('"imports.php" config file is not readable'));
+        return new static(__('"imports.php" config file is not readable'));
     }
 
     public static function deleteRunningImport()
     {
-        throw new static(__('The import is currently running and cannot be deleted'));
+        return new static(__('The import is currently running and cannot be deleted'));
 
     }
 
     public function fileNotReadable()
     {
-        throw new static(__('Unable to read file'));
+        return new static(__('Unable to read file'));
     }
 }
--- src/app/Exceptions/TemplateException.php
+++ src/app/Exceptions/TemplateException.php
@@ -10,7 +10,7 @@ class TemplateException extends EnsoException
 {
     public static function missingRootAttributes($attrs)
     {
-        throw new static(__(
+        return new static(__(
             'Attribute(s) Missing in template: ":attrs"',
             ['attrs' => $attrs]
         ));
@@ -18,7 +18,7 @@ class TemplateException extends EnsoException
     
     public static function missingSheetAttributes($attrs)
     {
-        throw new static(__(
+        return new static(__(
             'Mandatory Attribute(s) Missing in sheet object: ":attrs"',
             ['attrs' => $attrs]
         ));
@@ -26,7 +26,7 @@ class TemplateException extends EnsoException
 
     public static function unknownSheetAttributes($attrs)
     {
-        throw new static(__(
+        return new static(__(
             'Unknown Optional Attribute(s) in sheet object: ":attr"',
             ['attrs' => $attrs]
         ));
@@ -34,7 +34,7 @@ class TemplateException extends EnsoException
     
     public static function missingColumnAttributes($attrs)
     {
-        throw new static(__(
+        return new static(__(
             'Mandatory Attribute(s) Missing in column object: ":attr"',
             ['attrs' => $attrs]
         ));
@@ -42,7 +42,7 @@ class TemplateException extends EnsoException
     
     public static function unknownColumnAttributes($attrs)
     {
-        throw new static(__(
+        return new static(__(
             'Unknown Attribute(s) found in column object: ":attr"',
             ['attrs' => $attrs]
         ));
@@ -50,7 +50,7 @@ class TemplateException extends EnsoException
     
     public static function missingImporterClass(Obj $sheet)
     {
-        throw new static(__(
+        return new static(__(
             'Importer class ":class" for sheet ":sheet" does not exist',
             ['class' => $sheet->get('validatorClass'), 'sheet' => $sheet->get('name')]
         ));
@@ -58,7 +58,7 @@ class TemplateException extends EnsoException
     
     public static function importerMissingContract(Obj $sheet)
     {
-        throw new static(__(
+        return new static(__(
             'Importer class ":class" for sheet ":sheet" must implement the ":contract" contract',
             ['class' => $sheet->get('importerClass'), 'contract' => Importable::class]
         ));
@@ -66,7 +66,7 @@ class TemplateException extends EnsoException
 
     public static function missingValidatorClass(Obj $sheet)
     {
-        throw new static(__(
+        return new static(__(
             'Validator class ":class" for sheet ":sheet" does not exist',
             ['class' => $sheet->get('validatorClass'), 'sheet' => $sheet->get('name')]
         ));
@@ -74,7 +74,7 @@ class TemplateException extends EnsoException
     
     public static function incorectValidator(Obj $sheet)
     {
-        throw new static(__(
+        return new static(__(
             'Validator class ":class" for sheet ":sheet" must extend "LaravelEnso\DataImport\app\Services\Validators\Validator" class',
             ['class' => $sheet->get('validatorClass'), 'sheet' => $sheet->get('name')]
         ));
