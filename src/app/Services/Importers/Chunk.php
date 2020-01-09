222f560b4c6ed7165a407dabd7aad4d3263b0dfe
Vlad Chiovianu <vlad.chiovianu@earthlink.ro>
2020-01-09 18:18:29 +0200

    fixes custom validator and data import progress update

--- src/app/Services/Importers/Chunk.php
+++ src/app/Services/Importers/Chunk.php
@@ -46,7 +46,7 @@ class Chunk
         $this->index = $index;
         $this->rejected = new Collection();
         $this->importer = $this->template->importer($sheetName);
-        $this->validator = $this->template->customValidator($sheetName);
+        $this->validator = $this->template->customValidator($sheetName, $this->user, $this->params);
     }
 
     public function run(): void
@@ -108,8 +108,10 @@ class Chunk
 
     private function updateProgress(): void
     {
+        $this->dataImport->refresh();
+
         DB::transaction(fn () => DataImport::whereId($this->dataImport->id)
-            ->lockForUpdate()->first()
+            ->lockForUpdate()
             ->update([
                 'successful' => $this->dataImport->successful + $this->successful(),
                 'failed' => $this->dataImport->failed + $this->rejected->count(),
