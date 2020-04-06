1f9d17c743007fc26a6c4e3f31d86f83b1c87d84
mishu <mishu@xtelecom.ro>
2020-04-06 14:45:19 +0300

    fixes enum use in table

--- src/App/Tables/Builders/DataImportTable.php
+++ src/App/Tables/Builders/DataImportTable.php
@@ -5,7 +5,6 @@ namespace LaravelEnso\DataImport\App\Tables\Builders;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Facades\DB;
 use LaravelEnso\DataImport\App\Models\DataImport;
-use LaravelEnso\DataImport\App\Models\RejectedImportSummary;
 use LaravelEnso\Tables\App\Contracts\Table;
 
 class DataImportTable implements Table
@@ -15,21 +14,14 @@ class DataImportTable implements Table
     public function query(): Builder
     {
         return DataImport::selectRaw("
-            data_imports.id, data_imports.type, data_imports.status,
-            data_imports.status as computedStatus, files.original_name as name,
-            data_imports.successful, data_imports.failed, data_imports.created_at,
-            TIME(data_imports.created_at) as time, people.name as createdBy,
-            rejected_imports.id as rejectedId, {$this->rawDuration()} as duration
-        ")->join('files', fn ($join) => $join
-            ->on('files.attachable_id', 'data_imports.id')
-            ->where('files.attachable_type', DataImport::class)
-        )->join('users', 'files.created_by', '=', 'users.id')
-        ->join('people', 'users.person_id', '=', 'people.id')
-        ->leftJoin('rejected_imports', 'data_imports.id', '=', 'rejected_imports.data_import_id')
-        ->leftJoin('files as rejected_files', fn ($join) => $join
-            ->on('rejected_files.attachable_id', 'rejected_imports.id')
-            ->where('rejected_files.attachable_type', RejectedImportSummary::class)
-        );
+            data_imports.id, data_imports.type, data_imports.status, data_imports.status,
+            files.original_name as name, data_imports.successful, data_imports.failed,
+            data_imports.created_at, TIME(data_imports.created_at) as time,
+            people.name as createdBy, {$this->rawDuration()} as duration
+        ")->join('files', fn ($join) => $join->on('files.attachable_id', 'data_imports.id')
+            ->where('files.attachable_type', DataImport::class))
+            ->join('users', 'files.created_by', '=', 'users.id')
+            ->join('people', 'users.person_id', '=', 'people.id');
     }
 
     public function templatePath(): string
