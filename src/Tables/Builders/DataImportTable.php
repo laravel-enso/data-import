3df15bc95fe5583e33a2e31107b080f936b99bf7
reza <raftx24@gmail.com>
2020-09-28 12:25:33 +0330

    adds postgres queries

--- src/Tables/Builders/DataImportTable.php
+++ src/Tables/Builders/DataImportTable.php
@@ -17,7 +17,7 @@ class DataImportTable implements Table
         return DataImport::selectRaw("
             data_imports.id, data_imports.type, data_imports.status, files.original_name as name,
             data_imports.successful, data_imports.failed, data_imports.created_at,
-            TIME(data_imports.created_at) as time, people.name as createdBy,
+            {$this->rawTime()} as time, people.name as createdBy,
             rejected_imports.id as rejectedId, {$this->rawDuration()} as duration
         ")->join('files', fn ($join) => $join
             ->on('files.attachable_id', 'data_imports.id')
@@ -42,9 +42,18 @@ class DataImportTable implements Table
                 return $this->sqliteDuration();
             case 'mysql':
                 return $this->mysqlDuration();
+            case 'pgsql':
+                return $this->postgresDuration();
             default:
                 return 'N/A';
         }
+    }
+
+    private function rawTime(): string
+    {
+        return DB::getDriverName() === 'pgsql'
+            ? 'data_imports.created_at::time'
+            : 'TIME(data_imports.created_at)';
     }
 
     private function sqliteDuration()
@@ -60,5 +69,12 @@ class DataImportTable implements Table
         $seconds = 'timestampdiff(second, data_imports.created_at, data_imports.updated_at)';
 
         return "sec_to_time({$seconds})";
+    }
+
+    private function postgresDuration()
+    {
+        $seconds = 'EXTRACT(EPOCH FROM (data_imports.updated_at::timestamp- data_imports.created_at::timestamp ))';
+
+        return "($seconds || ' second')::interval";
     }
 }
