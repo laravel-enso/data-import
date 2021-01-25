0fa52545d5689ebb11be9a9af876207cf699da52
Manuela Mindroc <manuela.mindroc@earthlink.ro>
2021-01-25 11:28:28 +0200

    fixes ExcelSeeder

--- src/Services/ExcelSeeder.php
+++ src/Services/ExcelSeeder.php
@@ -4,18 +4,32 @@ namespace LaravelEnso\DataImport\Services;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 use LaravelEnso\DataImport\Models\DataImport;
 
 class ExcelSeeder extends Seeder
 {
     protected string $type;
     protected string $filename;
+    protected string $filePath;
     protected array $params = [];
 
     public function run()
     {
+        File::copy(
+            Config::get('enso.imports.seederPath').DIRECTORY_SEPARATOR.$this->filename,
+            Storage::path($this->filePath())
+        );
+
         DataImport::factory()
             ->make(['type' => $this->type, 'params' => $this->params])
-            ->attach(Config::get('enso.imports.seederPath'), $this->filename);
+            ->attach($this->filePath(), $this->filename);
+    }
+
+    private function filePath()
+    {
+        return $this->filePath ??= 'imports'.DIRECTORY_SEPARATOR.Str::random(40).'.xlsx';
     }
 }
