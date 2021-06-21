26184d0eef7f222776a47af5c43b30b3a77b2c81
mishu <mishu@xtelecom.ro>
2021-06-21 19:37:20 +0300

    adds params to optional attrs

--- src/Attributes/Template.php
+++ src/Attributes/Template.php
@@ -6,5 +6,5 @@ class Template extends Attributes
 {
     protected array $mandatory = ['sheets', 'timeout'];
 
-    protected array $optional = ['notifies'];
+    protected array $optional = ['notifies', 'params'];
 }
